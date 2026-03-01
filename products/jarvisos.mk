3740e8015b7baba288afa36866f42bb25ed343d3
Kevin Ocansey <kevinocansey11@gmail.com>
2026-03-01 18:42:29 +0000

    feat: add product makefile to include libcactus and libobjectbox-jni

---
+++ products/jarvisos.mk
@@ -0,0 +1,6 @@
+# JarvisOS product packages
+# Included by the device product config to pull JarvisOS additions into the system image.
+
+PRODUCT_PACKAGES += \
+    libcactus \
+    libobjectbox-jni
